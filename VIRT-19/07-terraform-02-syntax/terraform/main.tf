d65bc7d3fbad8fb3b1e5e1f2df0feb132ecea83b
Tokhir Abidov <abidovtf@yandex.ru>
2022-11-06 17:01:25 +0300

    readme

---
+++ VIRT-19/07-terraform-02-syntax/terraform/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = "key.json"
+  cloud_id  = "b1g9ihitksj8pk4q71d5"
+  folder_id = "b1giimrttnp35tj922g5"
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_vpc_network" "net" {
+  name = "net"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  network_id     = resource.yandex_vpc_network.net.id
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  zone           = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name        = "newinstance"
+  hostname    = "newinstance.local"
+  platform_id = "standard-v1"
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      type     = "network-hdd"
+      size     = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+    ipv6      = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
